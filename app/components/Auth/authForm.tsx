469dc950a88b2e768e1207ef5e4e09b7445a1f41
Thapelo12345 <79000570+Thapelo12345@users.noreply.github.com>
2025-08-09 11:25:20 +0200

    adding states

--- app/components/Auth/authForm.tsx
+++ app/components/Auth/authForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { UserCircleIcon } from "@heroicons/react/20/solid";
+import { SiFacebook, SiGoogle } from "react-icons/si";
 import { useState } from "react";
 import LabelInput from "./labelInput";
 
@@ -14,6 +15,7 @@ type Prop = {
 };
 
 export default function AuthForm({ sendData }: Prop) {
+  const [formState, setForm] = useState("logIn");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -25,36 +27,80 @@ export default function AuthForm({ sendData }: Prop) {
         e.preventDefault();
         sendData({ username, email, password, reEnter });
       }}
-      className="bg-gradient-to-t from-blue-600 to-blue-300 shadow-2xl shadow-black flex flex-col items-center rounded-lg justify-center border-2 border-white w-full sm:w-1/2 p-2 m-2"
+      className="bg-gradient-to-t from-black to-blue-300 shadow-2xl shadow-black flex flex-col items-center rounded-lg justify-center border-2 border-white w-full sm:w-1/2 p-2 m-2"
     >
       <UserCircleIcon className="w-30 h-20 text-white" />
 
       <LabelInput InputType="text" title="Usename" sendValue={setUsername} />
-      <LabelInput InputType="email" title="Email" sendValue={setEmail} />
+
+      {formState === "register" && (
+        <LabelInput InputType="email" title="Email" sendValue={setEmail} />
+      )}
+
       <LabelInput
         InputType="password"
         title="Password"
         sendValue={setPassword}
       />
-      <LabelInput
-        InputType="password"
-        title="Re-enter passwrd"
-        sendValue={setReEnter}
-      />
+      {formState === "register" && (
+        <LabelInput
+          InputType="password"
+          title="Re-enter passwrd"
+          sendValue={setReEnter}
+        />
+      )}
 
+      {/* socila media log in */}
+
+      <label className="text-white text-xs m-2">
+        Log In with your social media account
+      </label>
+      <div className="w-full flex flex-row items-center justify-center">
+        <button className="text-lg rounded-lg text-white hover:border-2 hover:border-white m-2 p-2">
+          <SiFacebook className="w-7 h-7" color="#0866FF" />
+        </button>
+
+        <button className="text-lg rounded-lg text-white hover:border-2 hover:border-white m-2 p-2">
+          <SiGoogle className="w-7 h-7" color="4285F4" />
+        </button>
+      </div>
+
+      {/* button logIn */}
       <div className="w-full flex flex-row justify-evenly">
-        <button
-          className="text-white self-start p-2 m-2 rounded-lg"
-          type="submit"
-        >
-          LogIn
-        </button>
-        <button className="text-white text-xs self-center p-2 m-2">
-          Forgot password
-        </button>
-        <button className="text-xs text-white self-end p-2 m-2 rounded-lg">
-          register an acc
-        </button>
+        {formState === "logIn" && (
+          <>
+            <button
+              className="text-white self-start p-2 m-2 rounded-lg"
+              type="submit"
+            >
+              LogIn
+            </button>
+            <button className="text-white text-xs self-center p-2 m-2">
+              Forgot password
+            </button>
+            <button
+              className="text-xs text-white self-end p-2 m-2 rounded-lg"
+              type="button"
+              onClick={() => setForm("register")}
+            >
+              register an acc
+            </button>
+          </>
+        )}
+
+        {formState === "register" && (
+          <>
+            <button className="text-white self-start p-2 m-2 rounded-lg">
+              Submit
+            </button>
+            <button
+              className="text-white self-start p-2 m-2 rounded-lg"
+              onClick={() => setForm("logIn")}
+            >
+              Back
+            </button>
+          </>
+        )}
       </div>
     </form>
   );
